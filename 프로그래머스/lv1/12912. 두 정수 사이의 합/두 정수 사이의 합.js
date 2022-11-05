f26572e0cf9d81f9a2057b3d9fb06501114bc29d
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2022-11-05 16:03:33 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    let answer = 0;
+    const numArr = a < b ? [a, b] : [b, a];
+    
+    for(let i = numArr[0]; i <= numArr[1]; i++){
+        answer += i;
+    }
+    return answer;
+}
