b8ebfcbccc60386e01f832614d1b76fd2ebe0a79
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2022-12-25 00:00:37 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,1 +1,15 @@
-const solution = (n, time) => new Array(time).fill(2).reduce((acc, multiply) => { return acc * multiply }, n)
+// 간단 풀이
+// time이 0(false)이 될 때까지 -1
+function solution(n, time) {
+    const multifly = 2;
+    
+    while(time) {
+        n = n * multifly;
+        time--;
+    }
+    return n;
+}
+
+// 복잡한 풀이
+// 초기값이 있는 reduce를 사용한 풀이(불필요)
+// const solution = (n, time) => new Array(time).fill(2).reduce((acc, multiply) => { return acc * multiply }, n)
