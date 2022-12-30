35e06fed8157c2fd4c36e8e943b921979740b564
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2022-12-31 00:00:32 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,16 +1,23 @@
 function solution(hp) {
-    var count = 0;
+    let count = 0;
     
-    while (hp) {
-        if(hp < 3){
-            hp --;
-        } else if (hp < 5) {
-            hp -= 3;
-        } else {
-            hp -= 5;
-        }
-        count++;
-    }
+    // 수학적으로 풀이
+    // 5로 나눈다. 5가 들어가는 횟수를 구한다.
+    // 5로 나눈 나머지를, 3으로 나눈다. 나눠지거나(1) 나눠지지(0) 않는 경우가 있음.
+    // 5로 나눈 나머지를, 3으로 나눈 나머지를 구한다. (2 ~ 0)
+    count = parseInt(hp / 5) + parseInt(hp % 5 / 3) + parseInt((hp % 5) % 3)
+    
+    // 지문 그대로 반복문
+    // while (hp) {
+    //     if(hp < 3){
+    //         hp --;
+    //     } else if (hp < 5) {
+    //         hp -= 3;
+    //     } else {
+    //         hp -= 5;
+    //     }
+    //     count++;
+    // }
     
     return count;
 }
