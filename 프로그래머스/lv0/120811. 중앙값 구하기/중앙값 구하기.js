796dba322f2b81a5677af0978ef18ad11d5b7e9b
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-01-11 00:18:52 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,5 +1,10 @@
 function solution(array) {
     const middleIdx = (array.length + 1) / 2 - 1;
+
+    // index 확인법 A.
+    // return array.sort((a, b)=>a-b)[middleIdx];
     
-    return array.sort((a, b)=>a-b)[middleIdx];
+    // index 확인법 B.
+    // 체이닝이 가독성이 더 높은 것 같습니다.
+    return array.sort((a, b)=>a-b).at(middleIdx);
 }
