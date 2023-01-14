625768da511cfeb62e7505ed45953db30ac178a4
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-01-14 23:13:20 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,4 +1,10 @@
 function solution(n) {
+    // 1. 풀이
+    // 사람(n)을 피자(7)로 나눔
+    // 나눈 수를 올림
+    // return Math.ceil(n / 7)
+    
+    // 2. 피자 먹기
     const pizza = 7;
     let count = 0;
     
