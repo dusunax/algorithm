089446099d3fe9373ee114135889f12ef73e7c60
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-04-19 22:16:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,5 @@
 function solution(s) {
-    let min = Math.min(...s.split(" "));
-    let max = Math.max(...s.split(" "));
+    const min = Math.min(...s.split(" "));
+    const max = Math.max(...s.split(" "));
     return `${min} ${max}`;
 }
