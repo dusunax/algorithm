4a4304d98ebdb4aea24adedcfb6ddff52f996469
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-04-27 23:16:01 +0900

    [unrated] Title: 추억 점수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,5 +1,7 @@
 /**
 * 1. for of 문과 indexOf 사용하기
+* - 소요시간 평균: 1.07ms
+* - 메모리 사용량 33.82MB
 */
 // function solution(name, yearning, photo) {
 //     const result = [];
@@ -21,23 +23,46 @@
 
 /** 
 * 2. for of 문과 map 사용하기
+* - 소요시간 평균: 1.01ms
+* - 메모리 사용량 평균: 33.8MB
+*/
+// function solution(name, yearning, photo) {
+//     const result = [];
+//     const yearningMap = new Map(name.map((key, idx) => [key, yearning[idx]]));
+
+//     for (const persons of photo) { // 이중 배열 photo를 순회
+//         let score = 0;
+        
+//         for (const person of persons) { // 각 사람 확인
+//             const nameIdx = name.indexOf(person); 
+//             if(nameIdx < 0) continue;           
+            
+//             score += yearningMap.get(person)
+//         }
+//         result.push(score);
+//     }
+
+//     return result;
+// }
+
+/**
+* 3. 객체와 reduce를 사용하기
+* - 소요시간 평균: 0.43ms
+* - 메모리 사용량 평균: 33.7MB
 */
 function solution(name, yearning, photo) {
-    const result = [];
-    const yearningMap = new Map(name.map((key, idx) => [key, yearning[idx]]));
+    const yearningObj = {};
 
-    for (const persons of photo) { // 이중 배열 photo를 순회
-        let score = 0;
-        
-        for (const person of persons) { // 각 사람 확인
-            const nameIdx = name.indexOf(person); 
-            if(nameIdx < 0) continue;           
-            
-            // score += yearning[nameIdx];
-            score += yearningMap.get(person)
-        }
-        result.push(score);
-    }
+    name.forEach((key, idx) => { // obj에 key과 value 설정
+        yearningObj[key] = yearning[idx];
+    });
 
-    return result;
+    return photo.map((persons) => {
+        return persons.reduce((score, person) => { 
+            // person이 yearningObj에 없다면 return
+            if (yearningObj[person] === undefined) return score;
+
+            return score += yearningObj[person];
+        }, 0); // score를 0으로 초기화
+    });
 }
