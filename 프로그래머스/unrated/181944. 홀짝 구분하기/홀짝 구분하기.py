2efd9dc4e268e7f912e5d7aa344343363e8fedbc
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-06-20 20:42:55 +0900

    [unrated] Title: 홀짝 구분하기, Time: 11.99 ms, Memory: 7.56 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+# 1. f-string 템플릿
+# print('{} is {}'.format(a, "even" if a % 2 == 0 else "odd"))
+
+# 2. if else문
+if a % 2 == 0:
+    result = "even"
+else:
+    result = "odd"
+    
+# print(f'{a} is {result}')
+    
+# 3. dict 자료형을 사용한 출력
+# ex) 자바스크립트의 object
+result_dict = {True: "even", False: "odd"}
+print(f'{a} is {result_dict[a % 2 == 0]}')
