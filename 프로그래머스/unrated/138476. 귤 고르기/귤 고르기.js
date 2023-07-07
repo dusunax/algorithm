4b143ea48633834af593f129a36d06d0a5634127
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-07-08 00:15:09 +0900

    [unrated] Title: 귤 고르기, Time: 268.36 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,27 @@
+/** 경수의 수 중, 귤 종류의 수가 최솟값인 경우 구하기 */
+// 귤의 갯수를 인덱스로 배열에 추가 => 배열을 오름차순으로 정렬 => 귤 갯수만큼 인덱스 이동
+// k: 팔고 싶은 귤 수
+// tangerine: 귤의 크기 배열
+function solution(k, tangerine) {
+    // 가장 큰 귤
+    const sizeCount = new Array(Math.max(...tangerine)+1).fill(0);
+    
+    // 귤 인덱스에 맞춰 갯수 세기
+    for(let i = 0; i < tangerine.length; i++) {
+        const current = tangerine[i];
+        sizeCount[current] += 1;
+    }
+    sizeCount.sort((a, b) => b-a);
+    
+    // k만큼 고르기
+    let index = 0;
+    let answer = 0;
+    while (k > 0) {
+        k -= sizeCount[index];
+        answer++; 
+        index++;
+    }
+    
+    // 계산이요
+    return answer;
+}
