b00e3b4d2f0db6fc90c661bc93db0fef490d31ba
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-13 23:07:19 +0900

    Time: 196 ms (6.02%), Space: 49.2 MB (5.08%) - LeetHub

---
+++ LeetCode/0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        console.log('leethub v2 testing 9')
+        const otherNumber = target - nums[i];
+        const idx = nums.indexOf(otherNumber);
+
+        if (idx !== -1 && idx !== i) return [i, idx];
+    }
+};
