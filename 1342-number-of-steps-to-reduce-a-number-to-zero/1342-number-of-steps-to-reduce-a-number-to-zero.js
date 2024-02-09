3ba18fdfb99aba4d7449eee764ba4e4762a5ef4c
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2024-02-08 19:55:44 -0800

    Time: 46 ms (87.39%), Space: 48.4 MB (30.91%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -1,22 +1,8 @@
-// JS
+// ----------------------------
+// Javascript
+// ----------------------------
 
 // A. modulo
-/**
- * @param {number} num
- * @return {number}
- */
-var numberOfSteps = function(num) {
-    let step = 0;
-
-    while (num) {
-        num % 2 ? num -= 1 : num /= 2;
-        step++;
-    }
-
-    return step;
-};
-
-// B. bitwise operations
 /**
  * @param {number} num
  * @return {number}
@@ -25,9 +11,25 @@ var numberOfSteps = function(num) {
 //     let step = 0;
 
 //     while (num) {
-//         num & 1 ? num -= 1 : num >>= 1;
+//         num % 2 ? num -= 1 : num /= 2;
 //         step++;
 //     }
 
 //     return step;
 // };
+
+// B. bitwise operations
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let step = 0;
+
+    while (num) {
+        num & 1 ? num -= 1 : num >>= 1;
+        step++;
+    }
+
+    return step;
+};
