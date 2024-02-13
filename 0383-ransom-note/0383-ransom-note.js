1e904f0e122b29932294bc483ebb1e493f7368d9
두선아 Dusuna <94776135+dusunax@users.noreply.github.com>
2024-02-13 06:42:22 -0800

    Time: 70 ms (76.92%), Space: 53.1 MB (16.32%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const magazinLetters = new Map();
+    
+    for(i of magazine) {
+        const currentCount = (magazinLetters.get(i) || 0)
+        magazinLetters.set(i, currentCount+1)
+    }
+
+    for(i of ransomNote) {
+        const currentCount = (magazinLetters.get(i) || 0)
+        if(!currentCount) return false;
+        
+        magazinLetters.set(i, currentCount-1)
+    }
+
+    return true;
+};
