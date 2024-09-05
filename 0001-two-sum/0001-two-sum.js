f14411b50f9ed8b797ac78015456813577f2ca85
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-05 12:28:56 +0900

    Time: 66 ms (53.96%), Space: 51.2 MB (34.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,4 +1,6 @@
-// O(N^2)
+// \U0001f449 Brute Force
+// Time complexity: O(n^2)
+// Space complexity: O(1)
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -17,13 +19,34 @@
 //     return result;
 // };
 //
+// var twoSum = function(nums, target) {
+//     let result = [];
+//     nums.forEach((num, idx) => {
+//         if(result.length === 2) return;
+//         const pair = target - num;
+//         const pairIndex = nums.findIndex(e => e === pair);
+//         if(pairIndex !== -1 && idx !== pairIndex) result = [idx, pairIndex]
+//     })
+//     return result;
+// };
+
+// \U0001f449 Two-pass Hash Table
+// Time complexity: O(n)
+// Space complexity: O(n)
 var twoSum = function(nums, target) {
     let result = [];
+
+    let numsMap = {};
+    for (let i = 0; i < nums.length; i++) {
+        numsMap[nums[i]] = i;
+    }
+
     nums.forEach((num, idx) => {
         if(result.length === 2) return;
-        const pair = target - num;
-        const pairIndex = nums.findIndex(e => e === pair);
-        if(pairIndex !== -1 && idx !== pairIndex) result = [idx, pairIndex]
+        const pairNum = target - num;
+        if(numsMap[pairNum] !== undefined && numsMap[pairNum] !== idx){
+            result = [numsMap[pairNum], idx]
+        }
     })
     return result;
 };
