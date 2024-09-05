3c05dda1228b2bc7078da8b07a330a528692d7a5
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-05 12:43:13 +0900

    Time: 58 ms (73.78%), Space: 51.3 MB (34.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -50,3 +50,16 @@ var twoSum = function(nums, target) {
     })
     return result;
 };
+// One-pass Hash Table
+// Time complexity: O(n)
+// Space complexity: O(n)
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {    
+        const pairNum = target - nums[i];
+        if(map.has(pairNum)) {
+            return [map.get(pairNum), i]
+        }
+        map.set(nums[i], i)
+    }
+};
