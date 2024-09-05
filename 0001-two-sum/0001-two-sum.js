35c44cb0d281b552f9585155db9e53db24625889
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-05 12:46:19 +0900

    Time: 50 ms (93.57%), Space: 51.9 MB (15.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -33,24 +33,25 @@
 // \U0001f449 Two-pass Hash Table
 // Time complexity: O(n)
 // Space complexity: O(n)
-var twoSum = function(nums, target) {
-    let result = [];
+// var twoSum = function(nums, target) {
+//     let result = [];
 
-    let numsMap = {};
-    for (let i = 0; i < nums.length; i++) {
-        numsMap[nums[i]] = i;
-    }
+//     let numsMap = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         numsMap[nums[i]] = i;
+//     }
 
-    nums.forEach((num, idx) => {
-        if(result.length === 2) return;
-        const pairNum = target - num;
-        if(numsMap[pairNum] !== undefined && numsMap[pairNum] !== idx){
-            result = [numsMap[pairNum], idx]
-        }
-    })
-    return result;
-};
-// One-pass Hash Table
+//     nums.forEach((num, idx) => {
+//         if(result.length === 2) return;
+//         const pairNum = target - num;
+//         if(numsMap[pairNum] !== undefined && numsMap[pairNum] !== idx){
+//             result = [numsMap[pairNum], idx]
+//         }
+//     })
+//     return result;
+// };
+
+// \U0001f449 One-pass Hash Table
 // Time complexity: O(n)
 // Space complexity: O(n)
 var twoSum = function(nums, target) {
