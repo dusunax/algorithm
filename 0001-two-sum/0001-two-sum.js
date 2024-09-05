7f027d9460c9fc228dda8c657bde187edc7492e8
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-05 12:18:53 +0900

    Time: 630 ms (5.09%), Space: 50.3 MB (54.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,26 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+//     const result = [];
+//     nums.forEach((num, idx) => {
+//         if(result.length === 2) return;
+//         nums.forEach((e, nestIdx) => {
+//             if(idx !== nestIdx){
+//                 if(num + e === target) result.push(idx);
+//             }
+//         })
+//     })
+//     return result;
+// };
+//
 var twoSum = function(nums, target) {
-    const result = [];
+    let result = [];
     nums.forEach((num, idx) => {
         if(result.length === 2) return;
-        nums.forEach((e, nestIdx) => {
-            if(idx !== nestIdx){
-                if(num + e === target) result.push(idx);
-            }
-        })
+        const pair = target - num;
+        const pairIndex = nums.findIndex(e => e === pair);
+        if(pairIndex !== -1 && idx !== pairIndex) result = [idx, pairIndex]
     })
     return result;
 };
