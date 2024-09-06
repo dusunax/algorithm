819d5ca0789eba8dc98e038e172c595f61dbd6ef
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-06 13:34:02 +0900

    Time: 38 ms (45.5%), Space: 16.4 MB (92.06%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,9 @@
 # // O(N)
-# // var plusOne = function(digits) {
-# //     const calcedNum = BigInt(digits.join("")) + BigInt(1);
-# //     return calcedNum.toString().split("")
-# // };
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        calcedNum = int(''.join(map(str, digits))) + 1
-        return [int(digit) for digit in str(calcedNum)]
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         calcedNum = int(''.join(map(str, digits))) + 1
+#         return [int(digit) for digit in str(calcedNum)]
+# //
 # var plusOne = function(digits) {
 #     for (let i = digits.length - 1; i >= 0; i--) {
 #         if (digits[i] < 9) {
@@ -18,4 +15,13 @@ class Solution:
 #     digits.unshift(1);
 #     return digits;
 # };
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        digits.insert(0, 1)
+        return digits
         
