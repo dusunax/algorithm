a2281ad19f921fcd37693267910bda73d208c76d
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-08 16:44:20 +0900

    Time: 143 ms (96.72%), Space: 28 MB (27.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,20 @@
 class Solution:
+    # time: O(N)
+    # space: O(N)
+    # def rotate(self, nums: List[int], k: int) -> None:
+    #     n = k % len(nums)
+
+    #     new_nums = nums[-n:] 
+    #     del nums[-n:] 
+        
+    #     nums[0:0] = new_nums 
+
+    # 역순 기법 Reversals 
+    # time: O(N)
+    # space: O(1), modify array in-place
     def rotate(self, nums: List[int], k: int) -> None:
         n = k % len(nums)
 
-        new_nums = nums[-n:] 
-        del nums[-n:] 
-        
-        nums[0:0] = new_nums 
+        nums.reverse()
+        nums[:n] = reversed(nums[:n])
+        nums[n:] = reversed(nums[n:])
