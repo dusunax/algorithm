d2f9325d86e38c0a8b9c1843932555c04ad04391
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-11 23:43:05 +0900

    Time: 46 ms (70.29%), Space: 16.8 MB (33.35%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         result = []
-        shorter, longer = (nums1, nums2) if len(nums1) < len(nums2) else (nums2, nums1)
-        counter = Counter(shorter)
+        counter = Counter(nums1) # Counter는 요소의 빈도를 세는 데 최적화된 딕셔너리 서브클래스입니다.
 
-        for num in longer:
+        for num in nums2:
             if counter[num] > 0:
                 counter[num] -= 1
                 result.append(num)
