a6406a6992f9e582fa4c8f1c016c7962da589dd9
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-11 23:26:35 +0900

    Time: 64 ms (25.07%), Space: 51.4 MB (33.79%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -9,12 +9,13 @@
 var intersect = function(nums1, nums2) { 
     const map = new Map();
     const result = [];
+    const [shorter, longer] = nums1.length < nums2.length ? [nums1, nums2] : [nums2, nums1];
     
-    for (const num of nums1) {
+    for (const num of shorter) {
         map.set(num, (map.get(num) || 0) + 1);
     }
 
-    for (const num of nums2) {
+    for (const num of longer) {
         if (map.has(num) && map.get(num) > 0) {
             map.set(num, map.get(num) - 1);
             result.push(num);
