71dceaf2af6c1d4c53318e7e176fd6efb6910616
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-12 14:38:53 +0900

    Time: 77 ms (40.05%), Space: 52.6 MB (75.06%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -2,13 +2,31 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    const reversedStr = Math.abs(x).toString().split('').reverse().join("");
-    const result = Number(reversedStr) * Math.sign(x);
+// Convert to string
+// var reverse = function(x) {
+//     const reversedStr = Math.abs(x).toString().split('').reverse().join("");
+//     const result = Number(reversedStr) * Math.sign(x);
 
-     if (result < Math.pow(-2, 31) || result > Math.pow(2, 31) - 1) {
-        return 0;
+//      if (result < Math.pow(-2, 31) || result > Math.pow(2, 31) - 1) {
+//         return 0;
+//     }
+
+//     return result;
+// };
+
+// Using remainder
+var reverse = function(x) {
+    let num = Math.abs(x)
+    let res = 0
+
+    while (num > 0) {
+        let digit = num % 10;
+        num = Math.floor(num / 10);
+        if (res > Math.floor((2 ** 31 - 1) / 10) || (res === Math.floor((2 ** 31 - 1) / 10) && digit > 7)) {
+            return 0;
+        }
+        res = (res * 10) + digit;
     }
 
-    return result;
-};
+    return res * Math.sign(x)
+}
