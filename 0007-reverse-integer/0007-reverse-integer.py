da49b9f964917019021d1e76b094e82be35bd9cf
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-12 14:48:08 +0900

    Time: 41 ms (23.88%), Space: 16.4 MB (80.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,12 +1,30 @@
 class Solution:
-    def reverse(self, x: int) -> int:
-        if x == 0:
-            return 0
+    # 1. Convert to string
+    # def reverse(self, x: int) -> int:
+    #     if x == 0:
+    #         return 0
             
-        reversed_str = str(abs(x))[::-1]
-        result = int(reversed_str) * (1 if x > 0 else -1)
+    #     reversed_str = str(abs(x))[::-1]
+    #     result = int(reversed_str) * (1 if x > 0 else -1)
 
-        if result < -2**31 or result > 2**31 - 1:
-            return 0
+    #     if result < -2**31 or result > 2**31 - 1:
+    #         return 0
 
-        return result
+    #     return result
+
+    # 2. Use Remainder
+    def reverse(self, x: int) -> int:
+        num = abs(x)
+        res = 0
+
+        while num > 0:
+            digit = num % 10
+            num //= 10
+
+            if res > ((2 ** 31 - 1) // 10) or ((res == 2 ** 31 - 1) // 10 and digit > 7):
+                return 0
+
+            res = res * 10 + digit
+
+        return res * (1 if x > 0 else -1)
+    
