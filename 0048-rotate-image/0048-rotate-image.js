5fa098b284a3eaa85136be7431b70a9c4cdb0d09
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-16 23:20:15 +0900

    Time: 47 ms (84.03%), Space: 49.4 MB (38.26%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -2,16 +2,19 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+// matrix rotation
+// by Transpose + Reverse
+// in-place 90도 회전 알고리즘
 var rotate = function (matrix) {
   const n = matrix.length;
 
   for (let i = 0; i < n; i++) {
     for (let j = i; j < n; j++) {
-      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]; // 행렬의 행(row)과 열(column)을 서로 교환
     }
   }
 
   for (let i = 0; i < n; i++) {
-    matrix[i].reverse();
+    matrix[i].reverse(); // 행렬의 각 행을 역순으로 바꿈
   }
 };
