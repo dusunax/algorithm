75df3e288859ff0aafbc6f692f505dfb8c5a8d45
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-17 20:19:43 +0900

    Time: 98 ms (23.61%), Space: 16.4 MB (98.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        n = len(board)
-        rows = [set() for _ in range(n)]
-        cols = [set() for _ in range(n)]
-        boxes = [set() for _ in range(n)]
-
-        for i in range(n):
-            for j in range(n):
+        rows, cols, boxes = [set() for _ in range(9)], [set() for _ in range(9)], [set() for _ in range(9)]
+    
+        for i in range(9):
+            for j in range(9):
                 num = board[i][j]
 
                 if num == '.':
                     continue
-                
-                if num in rows[i]:
-                    return False
-                rows[i].add(num)
 
-                if num in cols[j]:
-                    return False
-                cols[j].add(num)
-                
                 box_index = (i // 3) * 3 + (j // 3)
-                if num in boxes[box_index]:
+                
+                if num in rows[i] or num in cols[j] or num in boxes[box_index]:
                     return False
+                
+                rows[i].add(num)
+                cols[j].add(num)
                 boxes[box_index].add(num)
-
-                print(boxes)
+                
         return True
                     
