6dc0c75beb9394d204a9758bbbd01cf5a2c4d54b
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-18 13:45:48 +0900

    Time: 63 ms (6.05%), Space: 17 MB (46.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -11,26 +11,3 @@ class Solution:
             frequency[char] -= 1
         
         return True
-
-# /**
-#  * @param {string} s
-#  * @param {string} t
-#  * @return {boolean}
-#  */
-# var isAnagram = function(s, t) {
-#     if(s.length !== t.length) return false;
-#     const frequency = new Map();
-
-#     for (let char of s){
-#         frequency.set(char, (frequency.get(char) || 0) + 1)
-#     }
-
-#     for (let char of t){
-#         let match = frequency.get(char);
-#         if(!match) return false; // match can be undefined of zero
-
-#         frequency.set(char, match - 1)
-#     }
-
-#     return true
-# };
