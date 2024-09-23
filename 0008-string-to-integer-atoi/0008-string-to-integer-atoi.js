a19a87267519bd008155a89cc57f775a077214c6
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-23 21:08:31 +0900

    Time: 79 ms (60.45%), Space: 53.3 MB (75.12%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    let result = "";
+    let sign = 1
+    let index = 0;
+    const max = 2 ** 31 - 1;
+    const min = -(2 ** 31)
+
+    // Step 1: Whitespace, 공백 제거
+    s = s.trim();
+
+    // Step 2: Signedness, 부호 처리
+    if (s[index] === '-') {
+        sign = -1;
+        index++;
+    } else if (s[index] === '+') {
+        index++;
+    }
+
+    // Step 3: Conversion, 숫자 변환
+    while (index < s.length && s[index].match(/^\d$/)) {
+        result = result * 10 + Number(s[index]);
+        index++;
+
+        // Step 4: Rounding, 범위 초과
+        if (sign * result > max) {
+            return max;
+        }
+        if (sign * result < min) {
+            return min;
+        }
+    }
+    return (+result) * sign 
+};
