d5fd1c8a7aac855aeac13f98662c24f8387e4045
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-25 20:09:30 +0900

    Time: 20 ms (60.26%), Space: 11.8 MB (97.16%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -8,4 +8,15 @@ class Solution(object):
     def deleteNode(self, node):
         node.val = node.next.val
         node.next = node.next.next
-        
+
+# self
+# In Python, self refers to the instance of the class that is calling the method.
+# When defining methods inside a class, the first parameter of each method must be self
+
+# Why is "self" needed?
+# When you call a method on an instance of a class (for example, my_solution.deleteNode(node)), Python automatically passes the instance (my_solution) as the first argument to the method. By convention, this first argument is named self.
+
+# Why is self required even though it's not used inside deleteNode?
+# It's required by Python's syntax for all instance methods in a class.
+# It provides consistency across the code so that methods can always reference the instance they belong to, even if in this case it’s not strictly necessary.
+
