96c2d53e5e252e496856e4c6eae17444d8683874
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-09-30 00:17:17 +0900

    Time: 152 ms (5.2%), Space: 58.3 MB (5.37%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -10,26 +10,45 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+// <Approach 1. Iterative>
+// var mergeTwoLists = function(list1, list2) {
+//     let temp = new ListNode(-1); // memory
+//     let current = temp; // pointer
+
+//     while (list1 !== null && list2 !== null) {
+//         if(list1.val < list2.val){
+//             current.next = list1;
+//             list1 = list1.next;
+//         } else {
+//             current.next = list2;
+//             list2 = list2.next;
+//         }
+//         current = current.next;
+//     }
+
+//     if(list1 !== null){
+//         current.next = list1;
+//     } else if (list2 !== null) {
+//         current.next = list2;
+//     }
+
+//     return temp.next;
+// };
+
+// <Approach 2: Recursive>
 var mergeTwoLists = function(list1, list2) {
-    let temp = new ListNode(-1);
-    let current = temp;
-
-    while (list1 !== null && list2 !== null) {
-        if(list1.val < list2.val){
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
-        }
-        current = current.next;
+    if(list1 === null){
+        return list2;
+    } else if(list2 === null){
+        return list1;
     }
+        console.log(list1, list2)
 
-    if(list1 !== null){
-        current.next = list1;
-    } else if (list2 !== null) {
-        current.next = list2;
+    if(list1.val < list2.val){
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
-
-    return temp.next;
-};
+}
