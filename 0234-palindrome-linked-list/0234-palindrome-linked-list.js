e9a6fcfe78b91f6fd9c24a97d8cc176163e25999
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-10-03 01:19:56 +0900

    Time: 109 ms (90.29%), Space: 70.8 MB (66.27%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -15,13 +15,14 @@ var isPalindrome = function(head) {
     let slow = head;
     let fast = head;
     while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
+        slow = slow.next; // move 1 node
+        fast = fast.next.next; // move 2 node
     }
 
     let prev = null;
+    // reversing the second half of the list
     while (slow) {
-    let nextNode = slow.next;
+        let nextNode = slow.next; 
         slow.next = prev;
         prev = slow;
         slow = nextNode;
@@ -30,11 +31,11 @@ var isPalindrome = function(head) {
     let left = head;
     let right = prev;
     while (right) {
-    if (left.val !== right.val) {
-      return false;
-    }
-    left = left.next;
-    right = right.next;
+        if (left.val !== right.val) {
+            return false;
+        }
+        left = left.next;
+        right = right.next;
     }
 
     return true;
