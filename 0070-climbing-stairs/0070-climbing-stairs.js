476e61b51425a9f67eb20914efb03e665e698a85
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-11-04 19:51:33 +0900

    Time: 0 ms (100%), Space: 46.6 MB (100%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,30 +4,30 @@
  */
 var climbStairs = function(n) {
     // Bottom-up: iterate
-    // if (n === 1 || n === 2) return n;
+    if (n === 1 || n === 2) return n;
 
-    // let first = 1;
-    // let second = 2;
+    let first = 1;
+    let second = 2;
 
-    // for (let i = 3; i < n+1; i++){
-    //     const curr = first + second; // base case
-    //     first = second;
-    //     second = curr;
-    // }
-
-    // return second;
-
-    // Top-bottom: recursion
-    const memo = {}
-    const dp = (step) => {
-        if (step === 1 || step === 2) return step;
-        if (memo[step] !== undefined) return memo[step];
-        
-        memo[step] = dp(step -1) + dp(step-2);
-        return memo[step];
+    for (let i = 3; i < n+1; i++){
+        const curr = first + second; // base case
+        first = second;
+        second = curr;
     }
 
-    return dp(n)
+    return second;
+
+    // Top-bottom: recursion
+    // const memo = {}
+    // const dp = (step) => {
+    //     if (step === 1 || step === 2) return step;
+    //     if (memo[step] !== undefined) return memo[step];
+        
+    //     memo[step] = dp(step -1) + dp(step-2);
+    //     return memo[step];
+    // }
+
+    // return dp(n)
 };
 
 // basic DP problem
