5b7db8cc587929cbbc0053929d004d0394157f1c
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-11-04 20:09:21 +0900

    Time: 0 ms (100%), Space: 16.5 MB (41.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,36 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # 1. Bottom-up
-        if n is 1 or n is 2:
-            return n
+        # if n == 1 or n == 2:
+        #     return n
         
-        first = 1
-        second = 2
+        # first = 1
+        # second = 2
 
-        for i in range(3, n + 1):
-            curr = first + second
-            first = second
-            second = curr
+        # for i in range(3, n + 1):
+        #     curr = first + second
+        #     first = second
+        #     second = curr
         
-        return second
+        # return second
 
+        # 2. Top-bottom
+        memo = {}
+        def dp (self, step, memo) -> int:
+            if step == 1 or step == 2:
+                return step
+            if step in memo:
+                return memo[step]
+            memo[step] = dp(self, step -1, memo) + dp(self, step - 2, memo)
+            return memo[step]
+        
+        return dp(self, n, memo)
+
+# \U0001f4cc Python!
+# use `if step in memo``, not `if memo[step]``
+
+# Unnecessary self
+# - https://www.geeksforgeeks.org/self-in-python-class/
+# - you don’t need to include self in each call => it’s defined within the scope of the method climbStairs\U0001f914
+
+# use `==`` for comparison, not `is`
