488d949789c68e2e13b8576f5f8dfb562e7e83d9
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-05 01:40:40 +0900

    Time: 7 ms (54.29%), Space: 20.8 MB (43.63%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,15 +1,19 @@
 # return the k amount of element's array
 # frequencies = [{1: 3}, {2: 2}, {3: 1}]
 # return frequencies.slice(0, k)
-# 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # frequency_map = Counter(nums)
+        # sorted_frequencies = sorted(frequency_map.items(), key=lambda x: x[1], reverse=True)
+        # result = []
+
+        # for e in sorted_frequencies:
+        #     result.append(e[0])
+
+        # return result[0:k]
+
         frequency_map = Counter(nums)
-        sorted_frequencies = sorted(frequency_map.items(), key=lambda x: x[1], reverse=True)
-        result = []
 
-        for e in sorted_frequencies:
-            result.append(e[0])
+        return [item[0] for item in heapq.nlargest(k, frequency_map.items(), key=lambda x: x[1])]
 
-        return result[0:k]
