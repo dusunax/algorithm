38811165cdbdd1e07b171bb3e69b2a45a06e6925
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-10 20:56:21 +0900

    Time: 4 ms (27.68%), Space: 18.4 MB (19.2%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -5,6 +5,27 @@
 #         self.left = left
 #         self.right = right
 
+'''
+# Leetcode 108. Convert Sorted Array to Binary Search Tree
+
+- use recursive function to make the balanced binary search tree.
+
+## Time and Space Complexity
+
+```
+TC: O(n)
+SC: O(n)
+```
+
+### TC is O(n):
+- find the middle element (nums[center]) takes constant time = O(1)
+- each recursive step, since the size of the array is halved with each recursive step? = O(log n)
+- recursive function is called for each element in the array, so the total time complexity is O(n).
+
+### SC is O(n):
+- recursion stack space = O(log n)
+- space used by the result tree = O(n)
+'''
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         def makeBalancedBSTWithArray(nums) -> Optional[TreeNode]:
@@ -18,8 +39,4 @@ class Solution:
             middleNode.right = makeBalancedBSTWithArray(nums[center + 1:])
 
             return middleNode
-
         return makeBalancedBSTWithArray(nums)
-        
-        
-
