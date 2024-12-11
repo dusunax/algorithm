01aa88c8da738d7ea70ad26b2691b2f1fa537c13
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-11 21:33:46 +0900

    Time: 95 ms (52.35%), Space: 26.4 MB (30.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,15 @@ class Solution:
         min_price = prices[0]
         max_profit = 0
 
-        for i in range(len(prices)):
-            # min_price = min(prices[i], min_price)
-            if min_price > prices[i]:
-                min_price = prices[i]
-            max_profit = max(prices[i] - min_price, max_profit)
+        # for i in range(len(prices)):
+        #     if min_price > prices[i]:
+        #         min_price = prices[i]
+        #     max_profit = max(prices[i] - min_price, max_profit)
+
+        for p in prices[1:]:
+            if min_price > p:
+                min_price = p
+            max_profit = max(p - min_price, max_profit)
+            
         
         return max_profit
