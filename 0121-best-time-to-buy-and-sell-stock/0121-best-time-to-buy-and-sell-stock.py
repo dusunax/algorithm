2981eaccd3c3245e50fe100a4d40fae43d34e8b3
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-12 23:11:35 +0900

    Time: 106 ms (42.51%), Space: 26.5 MB (14.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,3 +1,21 @@
+'''
+# 121. Best Time to Buy and Sell Stock
+
+use **bottom-up dynamic programming** to solve the problem.
+
+## Time and Space Complexity
+```
+TC: O(n)
+SC: O(1)
+```
+
+## TC is O(n):
+- iterating through the list just once to find the maximum profit. = O(n)
+
+## SC is O(1):
+- using two variables to store the minimum price and maximum profit. = O(1)
+'''
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = prices[0]
@@ -7,10 +25,5 @@ class Solution:
             if min_price > prices[i]:
                 min_price = prices[i]
             max_profit = max(prices[i] - min_price, max_profit)
-
-        # for p in prices[1:]:
-        #     if min_price > p:
-        #         min_price = p
-        #     max_profit = max(p - min_price, max_profit)
         
         return max_profit
