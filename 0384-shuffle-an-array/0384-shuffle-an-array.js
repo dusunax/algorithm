e56b5328ce20440a6b83578bc969cc8db484807f
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-14 11:09:19 +0900

    Time: 20 ms (65.63%), Space: 63.7 MB (83.57%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+    this.array = [...nums];
+    this.origin = [...nums];
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+    this.array = [...this.origin]
+    return this.array;
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+    let shuffledArray = [...this.array];
+
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // Random index between 0 and i
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Swap elements
+    }
+    
+    this.array = shuffledArray;
+    return shuffledArray;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
