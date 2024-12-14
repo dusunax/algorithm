676fb79bc145188dfdef42cefe956f164f515764
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-14 10:48:49 +0900

    Time: 27 ms (60.2%), Space: 21.2 MB (14.03%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -1,27 +1,46 @@
 class Solution:
+    '''
+    1. use set & while loop
+    - first solution to think of.
+    '''
+    # def __init__(self, nums: List[int]):
+    #     self.array = nums    
+    #     self.origin = nums
+
+    # def reset(self) -> List[int]:
+    #     self.array = self.origin
+    #     return self.origin
+
+    # def shuffle(self) -> List[int]:
+    #     temp = []
+    #     unique_indices = set()
+
+    #     while len(temp) < len(self.array):
+    #         idx = random.randint(0, len(self.array) - 1)
+    #         if idx not in unique_indices:
+    #             temp.append(self.array[idx])
+    #             unique_indices.add(idx)
+
+    #     self.array = temp
+    #     return self.array
+
+    '''        
+    2. need to use shallow copy
+    - Fisher-Yates shuffle will do TC: O(n), SC: no additional memory
+    '''
     def __init__(self, nums: List[int]):
-        self.array = nums    
-        self.origin = nums
+        self.array = nums[:]
+        self.origin = nums[:]
 
     def reset(self) -> List[int]:
-        self.array = self.origin
+        self.array = self.origin[:]
         return self.origin
 
     def shuffle(self) -> List[int]:
-        temp = []
-        unique_indices = set()
-
-        while len(temp) < len(self.array):
-            idx = random.randint(0, len(self.array) - 1)
-            if idx not in unique_indices:
-                temp.append(self.array[idx])
-                unique_indices.add(idx)
-
-        self.array = temp
+        for i in range(len(self.array) - 1, 0, -1):
+            j = random.randint(0, i)
+            self.array[i], self.array[j] = self.array[j], self.array[i]
         return self.array
-            
-        
-
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
