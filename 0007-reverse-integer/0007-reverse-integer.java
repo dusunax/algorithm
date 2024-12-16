9bad16658a65d33a655f74174e0c8b1d533b8a51
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-17 00:01:37 +0900

    Time: 1 ms (85.95%), Space: 40.4 MB (97.79%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        int num = Math.abs(x);
+        int res = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            num /= 10;
+
+            // Check for overflow
+            if (res > (Integer.MAX_VALUE / 10) || (res == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            res = res * 10 + digit;
+        }
+
+        return x > 0 ? res : -res;
+    }
+}
