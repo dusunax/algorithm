61a2498556912453a15e2bea7e1b7b54782250d2
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-17 19:52:27 +0900

    Time: 18 ms (21.97%), Space: 17.8 MB (8.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,13 @@
 class Solution:
-    # <Use collections.Counter>
-    # Time complexity: O(N) + O(N) = O(N)
-    # - O(N) for Counter creation, O(N) for comparison
-    # Space complexity
-    # - Counter for s: O(N)
-    
-    # def isAnagram(self, s: str, t: str) -> bool:
-    #     return Counter(s) == Counter(t) 
-
-    # Manual frequency check
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
+
         frequency = Counter(s)
 
         for char in t:
-            if frequency[char] == 0 or char not in frequency:
+            if char not in frequency or frequency[char] == 0:
                 return False
             frequency[char] -= 1
-        
+
         return True
