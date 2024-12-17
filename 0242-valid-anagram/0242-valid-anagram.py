25b089cb3aab16e015b8f604698f29a89dc50316
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-17 20:26:02 +0900

    Time: 9 ms (79.35%), Space: 17.8 MB (9.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        '''
+        A. use frequency object
+        '''
+        # if len(s) != len(t):
+        #     return False
 
-        frequency = Counter(s)
+        # frequency = Counter(s)
 
-        for char in t:
-            if char not in frequency or frequency[char] == 0:
-                return False
-            frequency[char] -= 1
+        # for char in t:
+        #     if char not in frequency or frequency[char] == 0:
+        #         return False
+        #     frequency[char] -= 1
 
-        return True
+        # return True
+
+        '''
+        B. use Counter twice
+        '''
+        return Counter(s) == Counter(t)
