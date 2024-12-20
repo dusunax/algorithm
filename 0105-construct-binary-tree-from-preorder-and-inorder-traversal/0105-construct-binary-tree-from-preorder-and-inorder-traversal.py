515d7cd5c04b50fd7a8e70e4e90f351dff35e7c5
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-21 03:12:30 +0900

    Time: 35 ms (69.78%), Space: 18.9 MB (68.05%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,38 +6,57 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        inorder_index_map = {val: idx for idx, val in enumerate(inorder)}
-        pre_iter = iter(preorder)
-
-        def setTree(start, end):
-            if start > end:
+        def setTree(pre, start, end):
+            if not (pre < len(preorder) and start <= end):
                 return None
             
-            root_val = next(pre_iter) # iterate pre_iter for find root
-            root = inorder_index_map[root_val]
+            val = preorder[pre]
+            root = inorder.index(val)
+            left = setTree(pre + 1, start, root - 1)
+            right = setTree(pre + 1 + root - start, root + 1, end)
 
-            left = setTree(start, root - 1)
-            right = setTree(root + 1, end)
-            return TreeNode(root_val, left, right)
+            return TreeNode(preorder[pre], left, right)
         
-        return setTree(0, len(inorder) - 1)
-
-        # def setTree(pre_left, pre_right, in_left, in_right):
-        #     if pre_left > pre_right:
-        #         return None
+        return setTree(0, 0, len(inorder) - 1)
             
-        #     root_val = preorder[pre_left]
-        #     root = TreeNode(root_val)
 
-        #     root_index_in_inorder = inorder_idx_map[root_val] 
-        #     left_tree_size = root_index_in_inorder - in_left
+    '''
+    '''
+    # def buildTree3(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    #     inorder_index_map = {val: idx for idx, val in enumerate(inorder)}
+    #     pre_iter = iter(preorder)
 
-        #     root.left = setTree(pre_left + 1, pre_left + left_tree_size, in_left, root_index_in_inorder - 1)
-        #     root.right = setTree(pre_left + left_tree_size + 1, pre_right, root_index_in_inorder + 1, in_right)
-          
-        #     return root
+    #     def setTree(start, end):
+    #         if start > end:
+    #             return None
+            
+    #         root_val = next(pre_iter) # iterate pre_iter for find root
+    #         root = inorder_index_map[root_val]
 
-        # inorder_idx_map = {value: idx for idx, value in enumerate(inorder)}
+    #         left = setTree(start, root - 1)
+    #         right = setTree(root + 1, end)
+    #         return TreeNode(root_val, left, right)
+        
+    #     return setTree(0, len(inorder) - 1)
 
-        # return setTree(0, len(preorder) - 1, 0, len(inorder) - 1)
+    '''
+    '''
+    # def setTree(pre_left, pre_right, in_left, in_right):
+    #     if pre_left > pre_right:
+    #         return None
+        
+    #     root_val = preorder[pre_left]
+    #     root = TreeNode(root_val)
+
+    #     root_index_in_inorder = inorder_idx_map[root_val] 
+    #     left_tree_size = root_index_in_inorder - in_left
+
+    #     root.left = setTree(pre_left + 1, pre_left + left_tree_size, in_left, root_index_in_inorder - 1)
+    #     root.right = setTree(pre_left + left_tree_size + 1, pre_right, root_index_in_inorder + 1, in_right)
+        
+    #     return root
+
+    # inorder_idx_map = {value: idx for idx, value in enumerate(inorder)}
+
+    # return setTree(0, len(preorder) - 1, 0, len(inorder) - 1)
         
