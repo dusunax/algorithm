bf7657e7c2307e55dec6137b25c62422b81a5fef
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-25 20:06:50 +0900

    Time: 44 ms (11.82%), Space: 17.7 MB (5.91%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,10 +1,12 @@
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        bit = bin(n)[2:].zfill(32)
-        return int(bit[::-1], 2)
 
-        # result = 0
-        # for i in range(32):
-        #     result = (result << 1) | (n & 1)
-        #     n >>= 1
-        # return result
+class Solution:
+    # def reverseBitsA(self, n: int) -> int:
+    #     bit = bin(n)[2:].zfill(32)
+    #     return int(bit[::-1], 2)
+  
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for i in range(32):
+            result = (result << 1) | (n & 1) # shift the result to the left & add LSB of n
+            n >>= 1 # shift n to the right & remove previous LSB
+        return result
