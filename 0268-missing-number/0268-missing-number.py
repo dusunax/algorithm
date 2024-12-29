20c9a86eb896eee1c1800f4473e7e0513211086e
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-29 20:57:30 +0900

    Time: 3 ms (64.96%), Space: 18.9 MB (7.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,19 +1,57 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        '''
-        Iterative
-        '''
-        # nums.sort()
-        # n = len(nums)
-        
-        # for i in range(n):
-        #     if nums[i] != i:
-        #         return i
+'''
+# 268. Missing Number
 
-        # return n
-        '''
-        XOR
-        '''
+A. iterative approach: sort the array and find the missing number.
+B. XOR approach: use XOR to find the missing number.
+  - a ^ a = 0, a ^ 0 = a
+
+## Time and Space Complexity
+
+### A. Iterative Approach
+
+```
+TC: O(n)
+SC: O(1)
+```
+
+#### TC is O(n):
+- sorting the array. = O(n log n)
+- iterating through the array just once to find the missing number. = O(n)
+
+#### SC is O(1):
+- no extra space is used. = O(1)
+
+### B. XOR Approach
+
+```
+TC: O(n)
+SC: O(1)
+```
+
+#### TC is O(n):
+- iterating through the array just once to find the missing number. = O(n)
+
+#### SC is O(1):
+- no extra space is used. = O(1)
+
+'''
+class Solution:
+    '''
+    A. Iterative Approach
+    '''
+    def missingNumberIterative(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
+    
+    '''
+    B. XOR Approach
+    '''
+    def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         xor_all = 0
         xor_nums = 0
