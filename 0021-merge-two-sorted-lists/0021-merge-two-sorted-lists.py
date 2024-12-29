670ee3fdf99ae972f6adb173b6e3bc81594be637
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-29 20:52:58 +0900

    Time: 0 ms (100%), Space: 17.6 MB (17.2%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,10 +1,44 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+'''
+# 21. Merge Two Sorted Lists
+
+A. iterative approach: use a temp node to store the result.
+B. recursive approach: use recursion to merge the two lists.
+
+
+## Time and Space Complexity
+
+### A. Iterative Approach
+
+```
+TC: O(n + m)
+SC: O(1)
+```
+
+#### TC is O(n + m):
+- iterating through the two lists just once for merge two sorted lists. = O(n + m)
+
+#### SC is O(1):
+- temp node is used to store the result. = O(1)
+
+### B. Recursive Approach
+
+```
+TC: O(n + m)
+SC: O(n + m)
+```
+
+#### TC is O(n + m):
+- iterating through the two lists just once for merge two sorted lists. = O(n + m)
+
+#### SC is O(n + m):
+- because of the recursive call stack. = O(n + m)
+'''
 class Solution:
-    # <Approach 1. Iterative>
+    '''
+    A. Iterative Approach
+    - use a temp node to store the result.
+    - use a current node to iterate through the two lists.
+    '''
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         temp = ListNode(-1)
         current = temp
@@ -25,17 +59,19 @@ class Solution:
 
         return temp.next
 
-    # <Approach 2: Recursive>
-    # def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    #     if list1 is None:
-    #         return list2
-    #     elif list2 is None:
-    #         return list1
+    '''
+    B. Recursive Approach
+    - use recursion to merge the two lists.
+    '''
+    def mergeTwoListsRecursive(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
 
-    #     if list1.val < list2.val:
-    #         list1.next = self.mergeTwoLists(list1.next, list2)
-    #         return list1
-    #     else:
-    #         list2.next = self.mergeTwoLists(list1, list2.next)
-    #         return list2
-    
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
