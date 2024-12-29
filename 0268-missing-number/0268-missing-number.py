af310d2c3fbe1f946e7c8e26df85fcf1a04fcb6c
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-29 20:57:16 +0900

    Time: 7 ms (39.5%), Space: 18.7 MB (16.84%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,27 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
+        '''
+        Iterative
+        '''
+        # nums.sort()
+        # n = len(nums)
         
-        for i in range(n):
-            if nums[i] != i:
-                return i
+        # for i in range(n):
+        #     if nums[i] != i:
+        #         return i
 
-        return n
+        # return n
+        '''
+        XOR
+        '''
+        n = len(nums)
+        xor_all = 0
+        xor_nums = 0
+
+        for i in range(n + 1):
+            xor_all ^= i
+        
+        for num in nums:
+            xor_nums ^= num
+
+        return xor_all^xor_nums
