bb5e8e618378bb7d2038f452b19378606d6efeb9
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2024-12-31 00:00:32 +0900

    Time: 3117 ms (85.59%), Space: 18.1 MB (6.76%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
-        cols = len(board[0]) 
-
+        cols = len(board[0])
+        
         def backtracking(i, j, word_index):
             if word_index == len(word):
                 return True
-
+            
             if i < 0 or i >= rows or j < 0 or j >= cols or board[i][j] != word[word_index]:
-                return False 
+                return False
             
             temp_mark = board[i][j]
             board[i][j] = "."
-
+            
             found = (
-                backtrack(i + 1, j, word_index + 1) or
-                backtrack(i - 1, j, word_index + 1) or
-                backtrack(i, j + 1, word_index + 1) or
-                backtrack(i, j - 1, word_index + 1)
+                backtracking(i + 1, j, word_index + 1) or
+                backtracking(i - 1, j, word_index + 1) or
+                backtracking(i, j + 1, word_index + 1) or
+                backtracking(i, j - 1, word_index + 1)
             )
             
-            # Restore the cell
             board[i][j] = temp_mark
             
             return found
-
-        return backtracking(0,0,0)
-
+        
+        for row in range(rows):
+            for col in range(cols):
+                if backtracking(row, col, 0):
+                    return True
+        
+        return False
