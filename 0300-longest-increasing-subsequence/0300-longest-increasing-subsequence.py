ff51077354d9cb233762afac7cc873921a1b9477
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-01-17 15:23:03 +0900

    Time: 34 ms (73.33%), Space: 18.3 MB (9.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -3,10 +3,29 @@ class Solution:
         sub = []
         
         for num in nums:
-            pos = bisect.bisect_left(sub, num)
+            pos = self.bisectLeft(sub, num)
             if pos == len(sub):
                 sub.append(num)
             else:
                 sub[pos] = num
         
         return len(sub)
+
+    # <bisect.bisect_left> = TC: O(log n)
+    # finding the leftmost position in a sorted list
+    # where a given target value should be inserted to maintain the list's sorted order
+    def bisectLeft(self, list, target) -> int:
+        low = 0
+        high = len(list) - 1
+
+        while low <= high :
+            mid = int(low + (high - low) / 2) 
+
+            print()
+
+            if list[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
