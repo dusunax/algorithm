b3dd3413f8a85e81adf57b1f937d4d3ef2196e6e
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-01-25 02:45:02 +0900

    Time: 0 ms (100%), Space: 18.5 MB (37.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,6 +1,6 @@
+# reference: https://www.algodale.com/problems/set-matrix-zeroes/
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-      
+    def setZeroesWithSet(self, matrix: List[List[int]]) -> None:
         zero_rows = set()
         zero_cols = set()
 
@@ -17,3 +17,30 @@ class Solution:
         for c in zero_cols:
             for i in range(len(matrix)):
                 matrix[i][c] = 0
+
+    # def setZeroesWithVariable(self, matrix: List[List[int]]) -> None:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        first_row_has_zero = any(matrix[0][j] == 0 for j in range(cols))
+        first_col_has_zero = any(matrix[i][0] == 0 for i in range(rows))
+
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
+
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+
+        if first_row_has_zero:
+            for c in range(cols):
+                matrix[0][c] = 0
+
+        if first_col_has_zero:
+            for r in range(rows):
+                matrix[r][0] = 0
