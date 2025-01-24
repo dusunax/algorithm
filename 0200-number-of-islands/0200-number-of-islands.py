22db395b0ea32866c1ad463d770b71b6f09f31e8
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-01-24 23:48:49 +0900

    Time: 251 ms (53.39%), Space: 20.1 MB (66.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,9 +1,26 @@
+'''
+# 200. Number of Islands
+
+use DFS to find the number of islands (to find the all the possible cases)
+
+## Time and Space Complexity
+
+```
+TC: O(m * n)
+SC: O(m * n)
+```
+
+### TC is O(m * n):
+- dfs function is called for each cell in the grid for checking is the land ("1") = O(m * n)
+
+### SC is O(m * n):
+- using a recursive function, the call stack can go as deep as the number of cells in the grid in the worst case. = O(m * n)
+'''
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(x, y):
             if x < 0 or y < 0 or y >= len(grid) or x >= len(grid[0]) or grid[y][x] == "0" :
                 return
-            print(x, y, grid[y][x])
         
             grid[y][x] = "0"
             dfs(x, y + 1)
@@ -17,9 +34,5 @@ class Solution:
                 if grid[y][x] == "1":
                     dfs(x, y)
                     island_count += 1
-        
-        print(grid)
 
         return island_count
-
-        
