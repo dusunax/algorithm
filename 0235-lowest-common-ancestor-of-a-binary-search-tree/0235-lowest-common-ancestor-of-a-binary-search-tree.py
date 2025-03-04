7bfd5335d48f5ef15b9e57fce5da2ea98fc2e2b5
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-05 00:12:27 +0900

    Time: 65 ms (20.33%), Space: 21 MB (48.36%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,13 +7,25 @@
 '''
 
 기본적인 LCA 문제이다.
-- BST에서 주어진 두 노드의 Lowest(deepest) Common Ancestor 찾기
+- LCA는 BST에서 주어진 두 노드의 Lowest(deepest) Common Ancestor이다.
+- 두 노드를 descendants로 가진 노드이며, 두 노드도 포함된다.
 
-
+재귀 Bottom-up
+- p, q가 현재 노드보다 작으면, 왼쪽으로 이동
+- p, q가 현재 노드보다 크면, 오른쪽으로 이동
+- p, q가 현재 노드에서 양쪽으로 분리되어 나간다면, 현재 노드가 LCA이다.
 
 '''
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+    def lowestCommonAncestorTopDown(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while root:
             if p.val < root.val and q.val < root.val:
                 root = root.left
