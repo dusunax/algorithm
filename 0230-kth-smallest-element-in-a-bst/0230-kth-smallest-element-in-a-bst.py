726f274020621ad96a2aeed6735cd8440c3362ee
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-06 01:48:18 +0900

    Time: 6 ms (5.83%), Space: 21.1 MB (72.87%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,24 +5,48 @@
 #         self.left = left
 #         self.right = right
 '''
-- BST에서 k 번째의 값 찾기
+# BST에서 k 번째의 값 찾기
 
 - Inorder Traversal: 이진 탐색 트리를 중위 순회하면 트리의 모든 값을 오름 차순으로 방문할 수 있다.
+- k번째 가장 작은 값을 구하면 방문을 중단한다.
 '''
 class Solution:
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     result = []
+
+    #     def inorder(node):
+    #         if not node:
+    #             return
+    #         if len(result) > k:
+    #             return node
+
+    #         inorder(node.left)
+    #         result.append(node.val)
+    #         inorder(node.right)
+        
+    #     inorder(root)
+
+    #     return result[k - 1]
+
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        result = []
+        count = 0
+        result = None
 
         def inorder(node):
+            nonlocal count, result 
+
             if not node:
                 return
-            if len(result) > k:
+            
+            inorder(node.left)
+
+            count += 1
+            if count == k:
+                result = node.val
                 return
 
-            inorder(node.left)
-            result.append(node.val)
             inorder(node.right)
         
         inorder(root)
 
-        return result[k - 1]
+        return result
