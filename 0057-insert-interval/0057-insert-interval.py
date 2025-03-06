828f3758eaf4391fee0008a286ab7ab78fe96f11
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-06 22:51:26 +0900

    Time: 1 ms (66.61%), Space: 19.6 MB (61.56%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -15,7 +15,7 @@
 - SC: O(n)
 '''
 class Solution:
-    def insertUsingBisectLeftToFindIndex(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         start_idx = bisect_left([interval[0] for interval in intervals], newInterval[0]) # TC: O(log n)
         
         intervals.insert(start_idx, newInterval) # TC: O(n)
@@ -27,27 +27,4 @@ class Solution:
             else:
                 result[-1][1] = max(result[-1][1], interval[1])
             
-        return result
-    
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = []
-        i = 0
-        n = len(intervals)
-
-        # 1. insert until finding the correct index of newInterval
-        while i < n and intervals[i][1] < newInterval[0]:
-            result.append(intervals[i])
-            i += 1
-
-        # merge overapping intervals & insert newInterval
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
-            i += 1
-        result.append(newInterval)
-        
-        while i < n:
-            result.append(intervals[i])
-            i += 1
-        
         return result
