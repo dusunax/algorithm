a927bbd766fc7d9f8a2a52c518f3e339efffa951
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-12 17:44:08 +0900

    Time: 3 ms (94.55%), Space: 18.5 MB (59.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,9 @@
 class Solution:
     '''
-    brute force
+    ## brute force
+    - 전부 계산하기
     '''
-    def countBits(self, n: int) -> List[int]:
+    def countBitsBF(self, n: int) -> List[int]:
         result = []
 
         for i in range(int(n) + 1):
@@ -10,6 +11,21 @@ class Solution:
         
         return result
 
+    '''
+    ## DP
+    - 이전 계산 결과 기억하기
+    
+    ### 풀이
+    ```
+    이진수 = (이진수 >> 1) + (이진수 & 1)
+    ```
+    - `i >> 1`: i의 비트를 오른쪽으로 1비트 이동(맨 오른쪽 한 칸 버림), `i // 2`와 같음
+    - `i & 1`: `i`의 마지막 비트가 1인지 확인 (1이면 1 추가, 0이면 패스)
+    '''
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
         
-        
-        
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1) 
+            
+        return dp
