32f552eb755d2d00943202e61b765ebb97dc7000
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-12 17:43:54 +0900

    Time: 12 ms (36.02%), Space: 18.7 MB (22.88%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,7 +2,7 @@ class Solution:
     '''
     brute force
     '''
-    def countBitsBF(self, n: int) -> List[int]:
+    def countBits(self, n: int) -> List[int]:
         result = []
 
         for i in range(int(n) + 1):
@@ -10,16 +10,6 @@ class Solution:
         
         return result
 
-    '''
-    DP
-    '''
-    def countBits(self, n: int) -> List[int]:
-        dp = [0] * (n + 1)
-        
-        for i in range(1, n + 1):
-            dp[i] = dp[i >> 1] + (i & 1)
-            
-        return dp
         
         
         
