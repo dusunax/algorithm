535a7ed1070f2d659e9a40e14d5974517d4951b2
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-13 22:13:10 +0900

    Time: 3 ms (20.16%), Space: 18.5 MB (60.11%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,17 +1,57 @@
 '''
 # Leetcode 102. Binary Tree Level Order Traversal
 
-- travase BT
-- return the level order traversal
+do level order traversal
 
-## approach
-- travase with dep parameter => dp(node, dep)
-- store the travarse result
+```
+\U0001f4a1  why BFS is better in this case:
+- DFS doesn't naturally support level-by-level traversal, so we need an extra variable like "dep" (depth).
+- BFS is naturally designed for level traversal, making it a better fit for the problem.
+- additionally, BFS can avoid potential stack overflow.
+```
+
+## A. BFS
+
+### re-structuring the tree into a queue:
+- use the queue for traverse the binary tree by level.
+
+### level traversal:
+- pop the leftmost node
+- append the node's value to current level's array
+- enqueue the left and right children to queue
+- can only process nodes at the current level, because of level_size.
+
+## B. DFS
+- travase with a dep parameter => dp(node, dep)
+- store the traversal result
 '''
-
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
         result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+            result.append(level)
+
+        return result
+
+    def levelOrderDP(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = [] # O(n)
 
         def dp(node, dep):
             if node is None:
@@ -25,10 +65,6 @@ class Solution:
             dp(node.left, dep + 1)
             dp(node.right, dep + 1)
 
-        dp(root, 0)
+        dp(root, 0) # O(n)
 
         return result
-
-            
-
-        
