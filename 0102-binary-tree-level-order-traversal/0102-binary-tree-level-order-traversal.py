c4a395c3b71dc73326a9fe2f89f13ecd62e3574f
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-13 21:32:39 +0900

    Time: 0 ms (100%), Space: 18.5 MB (60.11%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,50 +1,34 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 '''
 # Leetcode 102. Binary Tree Level Order Traversal
 
-## Time and Space Complexity
+- travase BT
+- return the level order traversal
 
-```
-TC: O(n)
-SC: O(n)
-```
-
-### TC is O(n):
-- traversing through every node just once. O(n)
-
-### SC is O(n):
-- storing the result in a list. O(n)
+## approach
+- travase with dep parameter => dp(node, dep)
+- store the travarse result
 '''
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # level order traversal: [[], [], []]
-        # each nested arrays are a level.
         result = []
 
-         
-        # DFS for go through every node.
-        def traverseNode (node, level):
+        def dp(node, dep):
             if node is None:
                 return
 
-            if len(result) <= level:
-                result.append([])   
+            if len(result) <= dep:
+                result.append([])
 
-            if node.val is not None:
-                result[level].append(node.val)
+            result[dep].append(node.val)
             
-            if node.left is not None:
-                traverseNode(node.left, level + 1)
-            if node.right is not None:
-                traverseNode(node.right, level + 1)
+            dp(node.left, dep + 1)
+            dp(node.right, dep + 1)
 
-        traverseNode(root, 0)
-        
+        dp(root, 0)
+
         return result
+
+            
+
+        
