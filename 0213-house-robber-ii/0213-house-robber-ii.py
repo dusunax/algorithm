3d727254d7eff75cf4c5c2ac54f48b3d3da4ccf9
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-13 22:59:54 +0900

    Time: 3 ms (10.83%), Space: 17.8 MB (38.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,30 +4,30 @@
 '''
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-
-        def robbing(nums):
-            prev, maxAmount = 0, 0
-
-            for num in nums:
-                prev, maxAmount = maxAmount, max(maxAmount, prev + num)
-            
-            return maxAmount
-
-        return max(robbing(nums[1:]), robbing(nums[:-1]))
-            
     # def rob(self, nums: List[int]) -> int:
     #     if len(nums) == 1:
     #         return nums[0]
 
-    #     def robbing(start, end):
+    #     def robbing(nums):
     #         prev, maxAmount = 0, 0
 
-    #         for i in range(start, end):
-    #             prev, maxAmount = maxAmount, max(maxAmount, prev + nums[i])
+    #         for num in nums:
+    #             prev, maxAmount = maxAmount, max(maxAmount, prev + num)
             
     #         return maxAmount
 
-    #     return max(robbing(0, len(nums) - 1), robbing(1, len(nums)))
+    #     return max(robbing(nums[1:]), robbing(nums[:-1]))
+            
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def robbing(start, end):
+            prev, maxAmount = 0, 0
+
+            for i in range(start, end):
+                prev, maxAmount = maxAmount, max(maxAmount, prev + nums[i])
+            
+            return maxAmount
+
+        return max(robbing(0, len(nums) - 1), robbing(1, len(nums)))
