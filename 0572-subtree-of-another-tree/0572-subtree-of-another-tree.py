877f4ab5c8a27838abdb2956097ce32da5208f7b
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-03-18 21:59:49 +0900

    Time: 43 ms (60.21%), Space: 17.9 MB (84.15%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,22 +1,37 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+'''
+# 572. Subtree of Another Tree
+
+use a recursive function to check if the two trees are identical.
+- if they are identical, return True.
+- if they are not identical, recursively check the left subtree and right subtree.
+
+## base case
+- isSubtree
+  - if the current tree is None, return False.
+  - if the subRoot is None, return True. (empty tree is a subtree of any tree)
+- isIdentical
+  - if both trees are None, return True. (they are identical)
+  - if one of the trees is None, return False.
+  - if the values of the current nodes are different, return False.
+  - left subtree and right subtree's results are must be True.
+'''
 class Solution:
+    '''
+    TC: O(n * m), m is the number of nodes in the subRoot.
+    SC: O(n) (recursive stack space)
+    '''
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not root:
             return False
         if not subRoot:
             return True
 
-        if self.isIdentical(root, subRoot):
+        if self.isIdentical(root, subRoot): 
             return True
 
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    def isIdentical(self, s, t):
+    def isIdentical(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
         if not s and not t:
             return True
         if not s or not t:
