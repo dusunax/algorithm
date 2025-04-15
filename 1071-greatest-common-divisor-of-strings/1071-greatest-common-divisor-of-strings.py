dc93087672ac70a5f45a0e1a97326d39ab16b27a
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-04-15 21:55:00 +0900

    Time: 0 ms (100%), Space: 17.7 MB (92.25%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,8 @@
+# try all prefix?
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if not str1 + str2 == str2 + str1:
+            return ""
+        else:
+            return str1[:math.gcd(len(str1), len(str2))]
+            
