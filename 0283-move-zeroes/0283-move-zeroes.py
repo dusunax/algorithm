86e71074d1aca9f7451fa09d1f4072cb0bb09f96
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-18 12:04:09 +0900

    Time: 4 ms (62.86%), Space: 18.9 MB (49.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,18 +1,21 @@
 '''
 Key constraints:
 - move zeros to the end of the list
-- stable, in place => insertion sort
-
-insert sort
-- sorted array
-- pick up the target from unsorted array, iterate sorted array for comparsion, find the right place? insert it.
-
-key point
-- use write to overwrite el
-- after swapping, fills the ramaining tail with 0
+- stable, in place => insertion sort => Nope, because it's O(n²)
+- use pointer
 '''
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    '''
+    ### Overwrite+Fill
+    TC: O(n)
+    SC: O(1)
+
+    key point
+    - use `write` pointer to overwrite el
+    - after swaping, fills the ramaining tail with 0
+    '''
+    # def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroesOverwrite(self, nums: List[int]) -> None:
         write = 0
 
         for read in range(len(nums)):
@@ -23,8 +26,19 @@ class Solution:
         for i in range(write, len(nums)):
             nums[i] = 0
 
-        # last_non_zero_at = 0
-        # for current in range(len(nums)):
-        #     if nums[current] is not 0:
-        #         nums[last_non_zero_at], nums[current] = nums[current], nums[last_non_zero_at]
-        #         last_non_zero_at += 1
+    '''
+    ### In-Place Swap 
+    TC: O(n)
+    SC: O(1)
+
+    key point
+    - move in place, no two pass.
+    '''
+    # def moveZeroesInPlaceSwap(self, nums: List[int]) -> None:
+    def moveZeroes(self, nums: List[int]) -> None:
+        write = 0
+        for read in range(len(nums)):
+            if nums[read] is not 0:
+                if read != write:
+                    nums[write], nums[read] = nums[read], nums[write]
+                write += 1
