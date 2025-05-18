6e3988b566cf7dd811ff59c9ab5838d14e706d09
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-18 12:07:35 +0900

    Time: 4362 ms (5%), Space: 19 MB (31.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,10 +7,10 @@ Key constraints:
 class Solution:
     '''
     ### Overwrite+Fill
-    TC: O(n)
-    SC: O(1)
+    - TC: O(n)
+    - SC: O(1)
 
-    key point
+    key point:
     - use `write` pointer to overwrite el
     - after swaping, fills the ramaining tail with 0
     '''
@@ -28,17 +28,29 @@ class Solution:
 
     '''
     ### In-Place Swap 
-    TC: O(n)
-    SC: O(1)
+    - TC: O(n)
+    - SC: O(1)
 
-    key point
+    key point:
     - move in place, no two pass.
     '''
-    # def moveZeroesInPlaceSwap(self, nums: List[int]) -> None:
-    def moveZeroes(self, nums: List[int]) -> None:
+    # def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroesInPlaceSwap(self, nums: List[int]) -> None:
         write = 0
         for read in range(len(nums)):
             if nums[read] is not 0:
                 if read != write:
                     nums[write], nums[read] = nums[read], nums[write]
                 write += 1
+
+    '''
+    ### insertion sort like
+    - TC: O(n^2)
+    - SC: O(1)
+    '''
+    def moveZeroes(self, nums: List[int]) -> None:
+        for i in range(1, len(nums)): # total: O(n^2)
+            j = i
+            while j > 0 and nums[j - 1] == 0 and nums[j] != 0: # O(n), (bubbling: j to the left, index by index)
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                j -= 1
