12565887ad0d22cbb6f5be539c0deff42ea9fce4
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-19 23:31:44 +0900

    Time: 0 ms (100%), Space: 18 MB (5.33%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -6,9 +6,9 @@
 - s is much smaller than t: s is up to 100, t is up to 10^4
 
 ## approach
-- get the frequencies, compares them => x. need to compare "stable" way.
-- if iterate s, and find out each char is vaild? => SC will be O(n^2)
-- two pointer: use pointer for s & t
+- ❌ get the frequencies, compares them => x. need to compare "stable" way.
+- ❌ if iterate s, and find out each char is vaild by iterate t? => SC will be O(n^2)
+- ✅ two pointer: use pointer for s & t
 
 ## sudo
 ```
@@ -16,9 +16,9 @@ s_pointer = 0
 t_pointer = 0
 
 while s_pointer is smaller than s.lengh, and t_pointer is smaller than t.length
-    move t pointer until s pointer match.
-        move s_pointer +=1
-    move t_pointer +=1
+    check if t pointer and s pointer matches.
+        move s_pointer +=1 (found a match, move to next char in s)
+    move t_pointer +=1 (always forward in t)
     
     if s_pointer's pointer is same as s.len return true.
     else return false
@@ -27,11 +27,15 @@ while s_pointer is smaller than s.lengh, and t_pointer is smaller than t.length
 ''' 
 
 class Solution:
+    '''
+    TC: O(n)
+    SC: O(1)
+    '''
     def isSubsequence(self, s: str, t: str) -> bool:
         s_pointer = 0
         t_pointer = 0
 
-        while s_pointer < len(s) and t_pointer < len(t):
+        while s_pointer < len(s) and t_pointer < len(t): # O(len(t))
             if s[s_pointer] == t[t_pointer]:
                 s_pointer += 1
             t_pointer += 1
