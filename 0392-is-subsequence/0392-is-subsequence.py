c6821d3e069b788f3a75c01ef98e0d9e8052c3f8
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-19 23:27:22 +0900

    Time: 1 ms (37.78%), Space: 17.8 MB (67.38%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,42 @@
+'''
+## constraint
+- given two strings: s, t
+- they are lowercases.
+- return bool of if s is subsequence of t
+- s is much smaller than t: s is up to 100, t is up to 10^4
+
+## approach
+- get the frequencies, compares them => x. need to compare "stable" way.
+- if iterate s, and find out each char is vaild? => SC will be O(n^2)
+- two pointer: use pointer for s & t
+
+## sudo
+```
+s_pointer = 0
+t_pointer = 0
+
+while s_pointer is smaller than s.lengh, and t_pointer is smaller than t.length
+    move t pointer until s pointer match.
+        move s_pointer +=1
+    move t_pointer +=1
+    
+    if s_pointer's pointer is same as s.len return true.
+    else return false
+```
+- \U0001f4cc we are moving pointers, we can check the valid with where they pointed in the end.
+''' 
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_pointer = 0
+        t_pointer = 0
+
+        while s_pointer < len(s) and t_pointer < len(t):
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+            t_pointer += 1
+
+        return s_pointer == len(s)
+
+
+        
