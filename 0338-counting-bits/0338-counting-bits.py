c107fa21e9c20a85a8b0c836d83e660c05439bb8
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-25 19:56:06 +0900

    Time: 1 ms (98.65%), Space: 18.5 MB (59.34%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,23 +1,25 @@
 '''
-## 문제
+# 문제
 - 입력값: n
 - 반환: n+1 길이의 배열, 값은 index의 바이너리의 1의 개수
 
-## 풀이방식
-- bruthforce
-- divide the numbers in ranges
-- odd/even status
+# 풀이방식
+- bruteforce
+- dp: (Hint2)divide the numbers in ranges + (Hint3)odd/even status
 
-## 
+# DP 
+- how can we make a result, using small number to build the result of large number
+- dp[i >> 1] + (i % 2)
+- from 1 to n + 1
 '''
 
 class Solution:
     '''
-    ## Bruthforce
+    ## bruteforce
     - TC: O(n)
     - SC: O(n)
     '''
-    def countBits(self, n: int) -> List[int]:
+    def countBitsBF(self, n: int) -> List[int]:
         result = [0] * (n + 1)
 
         for i in range(len(result)):
@@ -26,24 +28,15 @@ class Solution:
             # int.bit_count() (Python 3.10 or later)
 
         return result
+    '''
+    ## DP
+    - TC: O(n)
+    - SC: O(n)
+    '''
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
 
-
-
-    # '''
-    # ## DP
-    # - 이전 계산 결과 기억하기
-    
-    # ### 풀이
-    # ```
-    # 이진수 = (이진수 >> 1) + (이진수 & 1)
-    # ```
-    # - `i >> 1`: i의 비트를 오른쪽으로 1비트 이동(맨 오른쪽 한 칸 버림), `i // 2`와 같음
-    # - `i & 1`: `i`의 마지막 비트가 1인지 확인 (1이면 1 추가, 0이면 패스)
-    # '''
-    # def countBits(self, n: int) -> List[int]:
-    #     dp = [0] * (n + 1)
-        
-    #     for i in range(1, n + 1):
-    #         dp[i] = dp[i >> 1] + (i & 1) 
+        for i in range(1, len(dp)):
+            dp[i] = dp[i >> 1] + (i % 2)
             
-    #     return dp
+        return dp
