f2c7cf1ced7d810dfd100cb6ac8d28474849bcdd
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-26 18:55:41 +0900

    Time: 3 ms (94.86%), Space: 18.1 MB (67.63%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -3,15 +3,22 @@ problem
 - find the int that doesn't exist on other array.
 
 approach
-- we dont checkout the index for making answer array. => conter method
+- we dont checkout the index for making answer array.
+- 각 중복 제거된 집합을 구하고, 각 배열에만 있는 원소를 구한다.
+
+집합
+- 차집합: -
+- 합집합: |
+- 교집합: &
+- 대칭차집합: ^
 '''
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        nums1set = set(nums1)
-        nums2set = set(nums2)
+        uniqueNums1 = set(nums1)
+        uniqueNums2 = set(nums2)
 
-        only1 = list(nums1set - nums2set)
-        only2 = list(nums2set - nums1set)
+        diff1 = list(uniqueNums1 - uniqueNums2)
+        diff2 = list(uniqueNums2 - uniqueNums1)
 
-        return [only1, only2]
+        return [diff1, diff2]
 
