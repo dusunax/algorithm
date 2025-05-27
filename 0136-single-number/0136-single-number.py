e16e937251035d22a674cf8f667922cc9adf32bc
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-05-28 08:09:47 +0900

    Time: 3 ms (66.05%), Space: 19.5 MB (94.68%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,12 @@
+'''
+- find the single integer among the duplicates
+- bitwise XOR op => will convert 0 to 1, 1 to 0
+- if we XOR the same number twice? => it cancels out, returns to before
+- so, all duplicates cancel each other out => XORing all integers, only the single number remains
+'''
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        while len(nums):
-            current = nums.pop()
-            if current not in nums:
-                return current
-            nums.remove(current)
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
