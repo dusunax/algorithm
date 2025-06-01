a010ceecf7cfeb4b06c1ecd7152872a13fa097e3
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-06-01 11:09:38 +0900

    Time: 2 ms (98.78%), Space: 42.4 MB (9.46%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -13,6 +13,7 @@ class Solution {
 
         return true;
     }
+    // imprement of Counter class in python
     public Map<Integer, Integer> intCounter(int[] arr) {
         Map<Integer, Integer> freqMap = new HashMap<>();
 
@@ -22,3 +23,29 @@ class Solution {
         return freqMap;
     }
 }
+
+/*
+### HashSet
+`set` is interface in Java. use a **concrete implementation** of the `Set` interface, like:
+
+- `HashSet` — unordered, backed by a hash table, best for average O(1) add/contains.
+- `TreeSet` — sorted set, backed by a tree (balanced), O(log n) operations.
+- `LinkedHashSet` — maintains insertion order.
+
+=> using set for fast lookup and no need for order => HashSet.
+
+### Entry in languages
+an Entry represents a key-value pair
+
+Java: 
+- Entry `Map.Entry`
+- use `.entrySet()` on map
+
+Python: 
+- tuple `(key, value)`
+- use `.items()`, on dict
+
+JavaScript: 
+- Array `[key, value]`
+- use `.entries()` on Object or map
+*/
