036adb54b8836763db64fabf11ffdc9ec5e2b34b
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-06-11 14:30:29 +0900

    Time: 0 ms (100%), Space: 19.2 MB (7.47%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,20 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+'''
+# DFS 
+'''
+# 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int: 
+    '''
+    TC: O(n), 모든 노드 방문
+    SC: O(h), 편향 트리 O(h), 균형 시 O(log n), n=h
+    '''
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        stack = [(root, 1)] 
         max_depth = 0
 
-        def helper(node, count):
-            nonlocal max_depth 
-            if node is None:
-                max_depth = max(max_depth, count)
-                return
-
-            helper(node.left, count+1) 
-            helper(node.right, count + 1)
-
-        helper(root, max_depth)
-
-        return max_depth
+        while stack:
+            (node, depth) = stack.pop()
+            if node:
+                max_depth = max(max_depth, depth)
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
                 
+        return max_depth
+
+    '''
+    TC: O(n), 모든 노드 방문
+    SC: O(h), 편향 트리 O(h), 균형 시 O(log n), n=h
+    '''
+    def maxDepthRecurrsion(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return max(left, right) + 1
+        
