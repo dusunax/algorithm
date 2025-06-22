622fc96840ccaff9bf24b8083e2d9da7d0852d0f
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-06-22 11:25:43 +0900

    Time: 0 ms (100%), Space: 17.9 MB (27.17%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+'''
+# Problem:
+- compare the leaf node sequences of 2 binary trees.
+- return True if the sequences are the same, otherwise return False.
+
+# Approach1: DFS twice
+- get the leaves of the 2 trees by DFS.
+- compare if they are same and return the boolean.
+
+# Approach2: DFS + BFS on the fly
+- get the leaves of the root1 tree.
+- using BFS, compare the root1 result with leaves of root2
+    - return false if they are not match, otherwise continue.
+- after compare, return true.
+'''
+class Solution:
+    '''
+    Approach1
+    '''
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        leaves1 = self.getLeaves(root1)
+        leaves2 = self.getLeaves(root2)
+
+        return leaves1 == leaves2
+      
+    def getLeaves(self, root: Optional[TreeNode]) -> List[int]:
+        leaves = []
+        self.dfs(root, leaves)
+        return leaves
+  
+    def dfs(self, node: TreeNode, leaves: List[int]) -> List[int]:
+        if not node:
+            return
+        if not node.left and not node.right:
+            leaves.append(node.val)
+        # no visited check, nothing to do unless it is a leaf node.
+        self.dfs(node.left, leaves)
+        self.dfs(node.right, leaves)
