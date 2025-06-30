bcafe799e3e6dfc66ee70f170ecb0e17d29ab75c
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-06-30 22:11:05 +0900

    Time: 45 ms (39.69%), Space: 17.7 MB (64.23%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,6 @@
 class Solution:
     # top down
-    def fibDP(self, n: int) -> int:
+    def fib(self, n: int) -> int:
         memo = {0: 0, 1: 1}
 
         def dp(n) -> int:
@@ -12,16 +12,16 @@ class Solution:
 
         return dp(n)
 
-    def fib(self, n: int) -> int:
-        f0, f1 = 0, 1
+    # def fib(self, n: int) -> int:
+    #     f0, f1 = 0, 1
 
-        if n == 0:
-            return f0
-        if n == 1:
-            return f1
+    #     if n == 0:
+    #         return f0
+    #     if n == 1:
+    #         return f1
 
-        for _ in range(2, n + 1):
-            f2 = f0 + f1
-            f0, f1 = f1, f2
+    #     for _ in range(2, n + 1):
+    #         f2 = f0 + f1
+    #         f0, f1 = f1, f2
 
-        return f1
+    #     return f1
