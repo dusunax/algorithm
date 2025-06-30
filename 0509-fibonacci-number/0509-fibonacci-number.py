1df0867c10902228ecf47bb58aa7fca67e926b18
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-06-30 22:10:25 +0900

    Time: 46 ms (37.68%), Space: 17.8 MB (23.59%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    # top down
+    def fibDP(self, n: int) -> int:
+        memo = {0: 0, 1: 1}
+
+        def dp(n) -> int:
+            if n in memo:
+                return memo[n]
+            memo[n] = dp(n - 1) + dp(n - 2)
+
+            return memo[n]
+
+        return dp(n)
+
+    def fib(self, n: int) -> int:
+        f0, f1 = 0, 1
+
+        if n == 0:
+            return f0
+        if n == 1:
+            return f1
+
+        for _ in range(2, n + 1):
+            f2 = f0 + f1
+            f0, f1 = f1, f2
+
+        return f1
