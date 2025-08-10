c1b79d91440278124a8ccac3e72fa806510e1644
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-08-10 12:03:43 +0900

    Time: 111 ms (21.78%), Space: 28.7 MB (23.96%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,5 +1,8 @@
+'''
 # 2 pointer
-# move the shorter pointer inward
+TC: O(n)
+SC: O(1)
+'''
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l = 0 
@@ -21,8 +24,17 @@ class Solution:
                 l += 1
             
         return max_area
-            
-        
 
-            
-        
+'''            
+# \U0001f4a1 Q&A
+
+## 1. Why move shorter pointer. 
+- Q. why do we move the *shorter* pointer inward?
+- A. the water level is limited by the shoter wall.
+    - we are *eliminate* the incorrect or irrelevant options in each steps, and moving taller pointer inward is incorrect move.
+    - if we keep the shorter wall and move taller pointer inward, width will decreases *while the limiting height stays the same or goes down*. so the max area of water (min(l, r) * width) can't improve.
+
+## 2. Can we stop moving pointers early?
+- Q. can't we stop early when the current max_area is smaller than previous max_area?
+- A. \U0001f645‍♀️ if we stop the working through the array early, we possibly missing the chance of way taller walls is exist in the *unknown inward*. so, we have to go through the whole array for checking all possible max_area. and it's O(n).
+'''
