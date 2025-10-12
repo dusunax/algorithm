1d3b4046baca56725b3f4a5366a1ed479f10e824
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-10-12 12:02:24 +0900

    Time: 75 ms (95.73%), Space: 18.3 MB (47.23%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -12,15 +12,21 @@
 - `Map` for count chars (+ multiset)
 '''
 class Solution:
+    '''
+    TC: O(n)
+    => py sorting op is `nlogn`, but, sorting(k) at most 26 numbers => constant O(1).
+    => so it is O(n)
+    SC: O(n)
+    '''
     def closeStrings(self, word1: str, word2: str) -> bool:
         # set: alphabet compare
-        s1 = set(word1)
-        s2 = set(word2)
+        s1 = set(word1) # TC: O(n)
+        s2 = set(word2) # same
         if s1 != s2:
             return False
 
         # map
-        c1 = Counter(word1)
+        c1 = Counter(word1) # TC: O(n)
         c2 = Counter(word2)
         return True if sorted(c1.values()) == sorted(c2.values()) else False
         
