31547d05e50993c2834fa9d8830aaa3a2c8452e0
Dusuna 두선아 <94776135+dusunax@users.noreply.github.com>
2025-11-23 11:45:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,32 @@
+'''
+## Problem
+- return longest common subsequenece's count
+- relative order is matters(stable)
+  - "ace" and "eca" has 1 LCS.
+
+## DP with LCS
+
+### DP table
+- init dp table with 0's
+- prev+1 when we find LCS: dp[i][j] = dp[i-1][j-1] + 1
+- keep max when if chars are not LCS: dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+### use bottom-up approach
+- since we know the table size upfront: (text1's len + 1) x (text2's len + 1), iterative DP is cleaner and efficient (no recurision depths.)
+'''
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = {}
+        len1 = len(text1)
+        len2 = len(text2)
 
-        def helper(i, j):
-            if i == len(text1) or j == len(text2):
-                return 0
+        dp_table = [[0] * (len2 + 1) for _ in range(len1 + 1)]
 
-            if (i, j) in dp:
-                return dp[(i, j)]
-
-            if text1[i] == text2[j]:
-                dp[(i, j)] = 1 + helper(i + 1, j + 1)
-            else:
-                dp[(i, j)] = max(helper(i + 1, j), helper(i, j + 1))
-
-            return dp[(i, j)]
-
-        return helper(0, 0)
+        for i in range(0, len1):
+            for j in range(0, len2):
+                if text1[i-1] == text2[j-1]:
+                    dp_table[i][j] = dp_table[i-1][j-1] + 1
+                else:
+                    dp_table[i][j] = max(dp_table[i-1][j], dp_table[i][j-1])
+        
+        return dp_table[len1-1][len2-1]
+        
